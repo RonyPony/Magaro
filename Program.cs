62c4a8145bf6c222a0b5de72a9084c263bad97fd
Ronel Cruz <ronel.cruz.a8@gmail.com>
2024-07-24 22:00:27 +0200

    WIP

--- Program.cs
+++ Program.cs
@@ -1,7 +1,89 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
+public class Program
     {
-        Console.WriteLine("Hello, World!");
+
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7080/chathub") // Asegúrate de que esta URL es correcta
+                .Build();
+
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine($"{user}: {message}");
+            });
+
+            await connection.StartAsync();
+            Console.WriteLine("Conectado a SignalR.");
+
+            while (true)
+            {
+                Console.Write("Ingrese su nombre: ");
+                var user = Console.ReadLine();
+
+                Console.Write("Ingrese su mensaje: ");
+                var message = Console.ReadLine();
+
+                await connection.InvokeAsync("SendMessage", user, message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al conectar: {ex.Message}");
+        }
     }
+
+
+
+
+    //public static string username = "";
+
+
+    //public static void jump(int chrs = 50)
+    //{
+    //    for (int i = 0; i < chrs; i++)
+    //    {
+    //        Console.Write("#");
+    //    }
+    //}
+    //public static void space()
+    //{
+    //    Console.WriteLine("");
+    //    Console.WriteLine("");
+    //    Console.WriteLine("");
+    //}
+    //public static void clean()
+    //{
+    //    Console.Clear();
+    //}
+    //private static void Main(string[] args)
+    //{
+    //    welcomeScreen();
+    //    Console.ReadKey();
+    //}
+
+    //private static void welcomeScreen()
+    //{
+    //    Console.WriteLine("Welcome to MAGARO");
+    //    jump();
+    //    Console.ReadKey();
+    //    selectUsername();
+    //}
+
+
+    //private static void selectUsername()
+    //{
+    //    Console.WriteLine("Please introduce your username");
+    //    space();
+    //    username = Console.ReadLine();
+
+    //}
+
 }
--- obj/Debug/net6.0/Magaro.AssemblyInfo.cs
+++ obj/Debug/net6.0/Magaro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magaro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b49c202da2885bdcb14719d4c1be4db8464f048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbfe04750ba0852a98e15bbfe998bc9a4f3443e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magaro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magaro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
